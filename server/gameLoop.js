832b2735f3154c0845ff2e5d465ced9200afdc01
Leonardo Lopes Lessa da Fonseca <leolopes00@hotmail.com>
2021-09-05 20:03:10 -0300

    Update gameLoop.js

--- server/gameLoop.js
+++ server/gameLoop.js
@@ -1,37 +1,37 @@
 
 class GameLoop{
     constructor(){              
-        this.addPlayer = function(){
-            player = new Player(this.map, this.ZOMBIES);
-            this.PLAYERS[player] = player;
-            return player;
-        }
-
-        this.spawnZombies = function(){
-            for(var i=0; i < this.hordeNum*2; i++){
-                zombie = new Zombie(this.map, this.PLAYERS);
-                this.ZOMBIES.push(zombie);
-            }
-        }
-    
-        this.update = function(){
-            if(this.ZOMBIES.lenght <= 0){
-                this.hordeNum++;
-                this.spawnZombies();
-            }
-
-            for(var zombie in this.ZOMBIES){
-                zombie.update();
-            }
-            for(var player in this.PLAYERS){
-                player.update();
-            }
-        }
-
         this.PLAYERS = {};
         this.ZOMBIES = [];
         this.map = {}
         this.hordeNum = 0;
+    }
+
+    addPlayer = function(){
+        player = new Player(this.map, this.ZOMBIES);
+        this.PLAYERS[player] = player;
+        return player;
+    }
+
+    spawnZombies = function(){
+        for(var i=0; i < this.hordeNum*2; i++){
+            zombie = new Zombie(this.map, this.PLAYERS);
+            this.ZOMBIES.push(zombie);
+        }
+    }
+
+    update = function(){
+        if(this.ZOMBIES.lenght <= 0){
+            this.hordeNum++;
+            this.spawnZombies();
+        }
+
+        for(var zombie in this.ZOMBIES){
+            zombie.update();
+        }
+        for(var player in this.PLAYERS){
+            player.update();
+        }
     }
 }
 
